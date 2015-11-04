74434564aa8fad32d1a6d0256a31dc1635680b68
Yuriy Timoshenkov <xsamadhix@gmail.com>
2015-11-05 00:53:37 +0200

    initial commit

---
+++ Huxley/Classes/Storage.fs
@@ -0,0 +1,34 @@
+﻿namespace Huxley.Storage
+
+type IStorage =
+    abstract member Add: string -> string -> unit
+    abstract member Get: string -> string
+    abstract member Name: string
+    abstract member Remove: string -> unit
+    abstract member CreateEmpty: string -> IStorage
+    abstract member GetContent: (string*string) list
+
+
+type MemoryStorage(name) =
+    
+    let mutable storage = Map.empty<string,string>;
+
+    interface IStorage with
+        member this.Add key value =
+            let storage = storage <- storage.Add (key, value)
+            printf "Added item with key = '%s' and value = '%s'" key value 
+
+        member this.Get key =
+            storage.[key]
+
+        member this.Name = name
+
+        member this.Remove key = 
+            storage <- storage.Remove key
+
+        member this.GetContent = storage |> Map.toList
+
+        member this.CreateEmpty name = new MemoryStorage(name) :> IStorage
+
+
+
---
+++ Huxley/Controllers/StorageController.fs
@@ -0,0 +1,48 @@
+﻿namespace Huxley.Controllers
+
+open System.Web.Http
+open Huxley.StorageManager
+open Huxley
+
+[<RoutePrefix("api/db")>]
+type StorageController(storageManager: IStorageManager) =
+    inherit ApiController() 
+
+    let storageManager = storageManager
+
+    new() = StorageController(Global.StorageManager)
+
+    [<Route("AddStorage/{name}")>]
+    [<HttpGet()>]
+    member this.AddStorage name = 
+        storageManager.Add name
+        this.Ok name
+
+    [<Route("RemoveStorage/{name}")>]
+    [<HttpGet()>]
+    member this.RemoveStorage name =
+        storageManager.Remove name
+        this.Ok name
+
+    [<Route("GetAllStorageNames")>]
+    [<HttpGet()>]
+    member this.GetAllStorageNames () =
+        storageManager.GetAllStorages |> Map.toList  |> List.map( fun (k,v) -> k ) |> this.Ok
+
+    [<Route("{dbName}/Add/{key}/{value}")>]
+    [<HttpGet()>]
+    member this.AddValue dbName key value = 
+        storageManager.GetAllStorages.[dbName].Add key value
+        this.Ok key
+
+    [<Route("{dbName}/Remove/{key}")>]
+    [<HttpGet()>]
+    member this.RemoveValue dbName key =
+        storageManager.GetAllStorages.[dbName].Remove key
+        this.Ok key
+
+    [<Route("{dbName}/GetContent")>]
+    [<HttpGet()>]
+    member this.GetDbContent dbName =
+        storageManager.GetAllStorages.[dbName].GetContent |> this.Ok
+
